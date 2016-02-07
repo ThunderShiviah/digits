b0ab810841d18e6aecb94fbf06e3d8d4668c6e03
Thunder <thunder@uoregon.edu>
2016-02-06 16:44:34 -0800

    Add ordered dict of experiment log params.

--- digits/data_logger.py
+++ digits/data_logger.py
@@ -2,6 +2,7 @@
 import json
 from common import data_io
 from time import strftime
+from pprint import pprint
 
 def log(clf, score):
     log_path = data_io.get_settings()["log_path"]
@@ -17,6 +18,29 @@ def log(clf, score):
         json.dump(log_obj, fp, 
                 sort_keys = True, indent = 4,
                 ensure_ascii=False)
+
+
+from collections import OrderedDict
+
+def make_log(clf, verbose=True):
+    
+    X_train, X_test, y_train, y_test = data_io.get_train_test_split() 
+    clf.fit(X_train, y_train)
+
+    log = OrderedDict({
+        "timestamp": strftime("%d-%m-%Y-%H-%M-%S"),
+        "model_name": clf.__str__(),
+        "model_params":clf.get_params(),
+        "training_data":data_io.get_settings()["train_data_path"],
+        "testing_data":data_io.get_settings()["test_data_path"],
+        "score": clf.score(X_test, y_test),
+        "model_file_handle": data_io.save_model(clf),
+            })
+
+    if verbose:
+        pprint(log)
+    
+    return log
 
 if __name__ == "__main__":
     log(1,1)
