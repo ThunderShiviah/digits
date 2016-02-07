e2de24881b7b44842fbeddb5e1d0a29fae0c6fd6
Thunder <thunder@uoregon.edu>
2016-02-06 16:45:33 -0800

    Refactor train to incorporate logger and pipeline

--- digits/train.py
+++ digits/train.py
@@ -1,28 +1,14 @@
 from common import data_io
-from sklearn.dummy import DummyClassifier
-from sklearn.cross_validation import train_test_split
-from sklearn.pipeline import make_pipeline
-
-X, y= data_io.get_train()
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-pipeline = make_pipeline(DummyClassifier(strategy='stratified', random_state=0),
-        )
-
+from data_logger import make_log
+from pipeline import get_pipeline
 
 if __name__ == "__main__":
 
-    from data_logger import log
+    model = get_pipeline()
 
-    pipeline.fit(X_train, y_train)
-    score = pipeline.score(X_test, y_test)
-    print(pipeline)
-    print(score)
-    print(pipeline.get_params())
+    print("--------------------Trying logging util----------")
+    make_log(model)
 
-    data_io.save_model(pipeline)
-    model = data_io.load_model()
-    print(model)
 
 
     #log(pipeline, score)
